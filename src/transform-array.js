4a8404aa2e31ae150837d49b0fbadd936abb2976
DrSavauG <dr.savaug@gmail.com>
2021-02-25 20:28:12 +0300

    feat: add solution for transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,26 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw Error;
+  let result = [];
+  let [din, dip, don, dop]=["--discard-next","--discard-prev","--double-next","--double-prev"];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == dip && arr[i - 2] != din) result.pop();
+    if (arr[i] == dop && arr[i - 2] != din) {
+      if (arr[i - 1] != undefined) {
+        result.push(arr[i - 1]);
+      }
+      i++;
+    }
+    if (arr[i] == don) {
+      if (arr[i + 1] != undefined) {
+        result.push(arr[i + 1]);
+      }
+      i++;
+    }
+    if (arr[i] == din) {
+      i++;
+      continue;
+    }
+    if ( arr[i] != dip &&arr[i] != dop &&arr[i] != undefined) result.push(arr[i]);
+  }
+  return result;
 };
