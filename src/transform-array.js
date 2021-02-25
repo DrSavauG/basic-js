b242859e7c1863bc0319e404443d778bb92d519f
DrSavauG <dr.savaug@gmail.com>
2021-02-25 20:49:14 +0300

    refactor: change some str of the project

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,26 +1,23 @@
 module.exports = function transform(arr) {
   if (!Array.isArray(arr)) throw Error;
-  let result = [];
-  let [din, dip, don, dop]=["--discard-next","--discard-prev","--double-next","--double-prev"];
+  let [result, din, dip, don, dop]=[[],"--discard-next","--discard-prev","--double-next","--double-prev"];
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == dip && arr[i - 2] != din) result.pop();
-    if (arr[i] == dop && arr[i - 2] != din) {
-      if (arr[i - 1] != undefined) {
+    if(arr[i - 2] != din){
+      if (arr[i] == dip)result.pop();
+      if(arr[i] == dop ){
         result.push(arr[i - 1]);
-      }
-      i++;
+        i++;
+      }  
     }
     if (arr[i] == don) {
-      if (arr[i + 1] != undefined) {
         result.push(arr[i + 1]);
-      }
       i++;
     }
     if (arr[i] == din) {
       i++;
       continue;
     }
-    if ( arr[i] != dip &&arr[i] != dop &&arr[i] != undefined) result.push(arr[i]);
+    if ( arr[i] != dip && arr[i] != dop ) result.push(arr[i]);
   }
-  return result;
+  return result.filter((e) => e !== undefined);
 };
