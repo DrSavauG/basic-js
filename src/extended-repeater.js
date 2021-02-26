f56f726b25f1cd0b8a7c8b7a5be95df755e8fa3d
DrSavauG <dr.savaug@gmail.com>
2021-02-26 13:18:42 +0300

    feat: add solution for extended-reaper

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,12 @@
 module.exports = function repeater(str, options) {
-
-
-};
-  
+  let [result, string, repeatTimes, separator, addition, additionRepeatTimes, additionSeparator] = ['', str + "", options.repeatTimes || 1, options.separator || "+", options.addition === undefined ? "" : options.addition + "", options.additionRepeatTimes || 1, options.additionSeparator || "|"];
+  for (let i = repeatTimes; i > 0; i--) {
+    result += string;
+    for (let j = additionRepeatTimes; j > 0; j--) {
+      result += addition;
+      if (j > 1) result += additionSeparator;
+    }
+    if (i > 1) result += separator;
+  }
+  return result;
+}
