7f1ff88253c1afacc8fd29acd8e1d105de0fc19a
DrSavauG <dr.savaug@gmail.com>
2021-03-12 15:34:23 +0300

    init: start vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,6 +1,7 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
+  constructor(direct){
+    this.direct = direct;
+  }
   encrypt() {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
