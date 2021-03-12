9b1e3c61533432f91c46576e46e6c4bf3cdb653f
DrSavauG <dr.savaug@gmail.com>
2021-03-12 19:28:46 +0300

    refactor: rename vars, delete comments

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -8,45 +8,25 @@ class VigenereCipheringMachine {
   decrypt(string, key) {
     return this.engine(string, key, true);
   }
-
   engine(string, key, mode = false) {
     if (!string || !key) throw Error();
-    let result = [];
-    let abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     string = string.toUpperCase();
     key = key.toUpperCase();
-    let str_l = string.length;
-    let abc_l = abc.length;
-    for (let i = 0,j = 0; i < str_l; i++) {
-      if(abc.includes(string[i])){
+    let abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    let result = [],
+      abc_l = abc.length;
+    for (let i = 0, j = 0,sign = 0; i < string.length; i++) {
+      if (abc.includes(string[i])) {
         let str_i = abc.indexOf(string[i]);
         let key_i = abc.indexOf(key[j]);
-        if(mode){
-          
-          result.push(abc[(str_i - key_i + abc_l) % abc_l]) //decrypt
-        }else {
-          result.push(abc[(str_i + key_i) % abc_l])// encrypt
-        }
-        
-        if(j === key.length - 1){
-          j = 0;
-        }else j++;
-      }else{
+        mode ? sign = -1 : sign = +1;
+        result.push(abc[(str_i + (key_i * sign) + abc_l) % abc_l]);
+        j = ++j % key.length;
+      } else {
         result.push(string[i])
       }
-
     }
     return this.direct ? result.join('') : result.reverse().join('');
   }
 }
-
 module.exports = VigenereCipheringMachine;
-
-// ////////////////////////////////////////////////////////////////
-
-
-// const directMachine = new VigenereCipheringMachine();
-// // directMachine.encrypt('1', '2');
-// // directMachine.decrypt(3, 4);
-// console.log( directMachine.encrypt('Same length key', 'Samelengthkey'), 'KAYI WIAMMO UIW');
-// console.log(directMachine.decrypt('HSVD AJAL ^^', 'behappy'), 'decr = GOOD LUCK ^^')
